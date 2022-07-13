992b7edb75a93bae5bf62f0e7dc76611a2fda505
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-07-13 20:51:48 -0300

    API test

---
+++ backend/src/main/java/com/thiago/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.thiago.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiago.dsmeta.entities.Sale;
+import com.thiago.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+
+}
